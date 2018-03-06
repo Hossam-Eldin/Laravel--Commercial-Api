63d98c615ab2c6a2e368c47e38fbf09064b4c1b1
Hossam-Elden <hosam.elden@hotmail.com>
2018-03-06 21:23:00 +0200

    relationships models

--- app/Category.php
+++ app/Category.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
+use App\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+    	'name',
+    	'discription'
+    ];
+
+    public function products(){
+    	retrun $this->belongsToMany(Product::class);
+    }
+
 }
