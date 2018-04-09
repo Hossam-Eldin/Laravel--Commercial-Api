5bcab0f01a202970571e60f571aeaf00603854ab
Hossam-Elden <hosam.elden@hotmail.com>
2018-04-09 11:12:50 +0200

    sorting and filtering data

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -19,6 +19,10 @@ trait ApiResponser{
 			return $this->successResponse(['data' => $collection], $code);
 		}
 		$transformer = $collection->first()->transformer;
+
+        
+        $collection = $this->filterData($collection, $transformer);
+        $collection = $this->sortData($collection,$transformer);
 		$collection = $this->transformData($collection, $transformer);
         
         return $this->successResponse($collection, $code);
@@ -30,9 +34,38 @@ trait ApiResponser{
         
         return $this->successResponse( $instance, $code);
     }
+
+
     protected function showMessage($message ,$code =200){
         return $this->successResponse(['data'=> $message], $code);
     }
+
+
+    //filter data based on value
+    protected function filterData(Collection $collection, $transformer)
+    {
+        foreach (request()->query() as $query => $value) {
+            $attribute = $transformer::originalAttribute($query);
+            if (isset($attribute, $value)) {
+                $collection = $collection->where($attribute, $value);
+            }
+        }
+        return $collection;
+    }
+
+
+    //data sorting  with transformers
+    public function sortData(Collection $collection , $transformer){
+        if (request()->has('sort_by')) {
+            //$attribute = request()->sort_by;
+            $attribute = $transformer::originalAttribute(request()->sort_by);
+
+            $collection = $collection->sortBy->{$attribute};
+        }
+        return $collection;
+    }
+
+
 
     protected function transformData($data, $transformer)
 	{
