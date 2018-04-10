62b645b089c14358ad35636f887a949a56e76f88
Hossam-Elden <hosam.elden@hotmail.com>
2018-04-10 14:14:11 +0200

    pageintion and cashe the query

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -2,7 +2,10 @@
 namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser{
     private  function successResponse($data , $code){
@@ -23,7 +26,10 @@ trait ApiResponser{
         
         $collection = $this->filterData($collection, $transformer);
         $collection = $this->sortData($collection,$transformer);
+        $collection = $this->paginate($collection);
 		$collection = $this->transformData($collection, $transformer);
+        $collection = $this->cacheResponse($collection);        
+
         
         return $this->successResponse($collection, $code);
     }
@@ -39,7 +45,25 @@ trait ApiResponser{
     protected function showMessage($message ,$code =200){
         return $this->successResponse(['data'=> $message], $code);
     }
-
+    // paginate Api custome number 
+    protected function paginate(Collection $collection)
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50',
+        ];
+        Validator::validate(request()->all(), $rules);
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 15;
+        if (request()->has('per_page')) {
+            $perPage = (int) request()->per_page;
+        }
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+        $paginated->appends(request()->all());
+        return $paginated;
+    }
 
     //filter data based on value
     protected function filterData(Collection $collection, $transformer)
@@ -55,7 +79,7 @@ trait ApiResponser{
 
 
     //data sorting  with transformers
-    public function sortData(Collection $collection , $transformer){
+    protected function sortData(Collection $collection , $transformer){
         if (request()->has('sort_by')) {
             //$attribute = request()->sort_by;
             $attribute = $transformer::originalAttribute(request()->sort_by);
@@ -66,10 +90,23 @@ trait ApiResponser{
     }
 
 
-
+    //transform database values
     protected function transformData($data, $transformer)
 	{
 		$transformation = fractal($data, new $transformer);
 		return $transformation->toArray();
 	}
+
+    //cashe function working with query
+    protected function cacheResponse($data)
+    {
+        $url = request()->url();
+        $queryParams = request()->query();
+        ksort($queryParams);
+        $queryString = http_build_query($queryParams);
+        $fullUrl = "{$url}?{$queryString}";
+        return Cache::remember($fullUrl, 30/60, function() use($data) {
+            return $data;
+        });
+    }
 }
